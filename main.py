ebc8f6e9f1a1e6c947b376e520075a87875eb857
durikang <kooda21@naver.com>
2024-11-13 13:47:15 +0900

    hello world

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui.gui import FinancialApp
-# V3 vv
+# v444
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = FinancialApp()
